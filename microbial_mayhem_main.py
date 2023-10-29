9de3add9c522c59a54beb046ae7271ede02655c3
pfb11 <pfb11@info11.cshl.edu>
2023-10-29 11:28:57 -0400

    adding in function stuffs

--- microbial_mayhem_main.py
+++ microbial_mayhem_main.py
@@ -2,9 +2,9 @@
 
 #this is the file where the main scripting will go
 #it is NOT the class file, it is NOT a method/function file
-import colony_size
 
-#The following lines import all of our methods in our class
+#The following lines import all of our methods into our main file
+import colony_size
 import sec_sys
 import math
 import json
@@ -15,7 +15,7 @@ def main():
   print('Welcome to Microbial Mayhem!\nChoose two microbes to fight!')
   user_in = input('Press o +return for microbe options: ')
   if user_in == 'o':
-    print('M. tuberculosis\nV. paramaris\nA. nidulans')
+    print('M.tuberculosis\nV.paramaris\nM.alcalica\nS.aureaus\nV.neptunius\nP.fluorescens\nK.pneumoniae\nE.coli')
     #define microbe a
     microbe_a_species = input('Type in your choice for Microbe A: ')
     #should be an integer value, from 1-100 
@@ -37,23 +37,34 @@ def main():
 
   #now, call class attributes - working on this now
   #Need growth rate and kin selection from dictionary that Clare and Tiffany are making 
-  microbe_A_def = Microbe(microbe_a_species,[ENTER GROWTH RATE],[ENTER KIN SELECT])
-  microbe_B_def = Microbe(microbe_b_species,[ENTER GROWTH RATE],[ENTER KIN SELECT])
+  microbe_A_def = Microbe(microbe_a_species,1,1)
+  microbe_B_def = Microbe(microbe_b_species,2,2)
   
+
+#Who wins? A or B?
+#function needs (), but values 
+microbeA_test = 
+print(f'microbeA_test')
+#microbe_a_total = microbe_A_def.BGC_content() + microbe_A_def.growth_Rate + microbe_a_colony
+#microbe_b_total = 0
+
+#if microbe_a_total > microbe_b_total:
+ # print ('Microbe A wins!')
+#if microbe_b_total > microbe_a_total:
+ # print('Microbe B wins!')
+#if microbe_a_total == microbe_b_total:
+ # print('Microbe A and B both survived!')
+
 
 
 #this is the battle function, which compares the score of microbe A and microbe B
-  def battle(microbeA,microbeB):
-    if microbeA.compete > microbeB.compete:
-      return microbeA
-    elif microbeB.compete > microbeA.compete:
-      return microbeB
-    else:
-      return microbeA, microbeB    
-#After A and B fight
-#microbe_a_total =
-#microbe_b_total =
-
+#  def battle(microbeA,microbeB):
+ #   if microbeA.compete > microbeB.compete:
+  #    return microbeA
+   # elif microbeB.compete > microbeA.compete:
+   #   return microbeB
+   # else:
+    #  return microbeA, microbeB    
 
 if __name__ == "__main__":
   main()
