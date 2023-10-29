1bb335ff49764bfc1430620377f3c1b1890c623e
pfb23 <pfb23@info23.cshl.edu>
2023-10-29 10:02:16 -0400

    dding a former version of the file that I previously modified

--- microbial_mayhem_main.py
+++ microbial_mayhem_main.py
@@ -2,6 +2,7 @@
 
 #this is the file where the main scripting will go
 #it is NOT the class file, it is NOT a method/function file
+import colony_size
 
 def microbe_input(microbe_str):
   variable = ''
@@ -23,16 +24,18 @@ def main():
     microbe_a_env = input('Please enter one of the following environments: Alkaline, Hot, Cold, Acidic, Salty: ')
     #should be an integer value, from 1-100 
     microbe_a_colony = input('Please enter the size of your colony, size 1-1000: ') 
+    microbe_a_colony_zise=colony_size.colony_growth()
     #a yes or a no
     microbe_a_sec = input('Does your microbe have a secretion system? Yes or No?: ')
  
     #now, define b
     microbe_b_species = input('Type in your choice for Microbe B: ')
     #the user should input one of the 5 options
-    microbe_b_env = input('Please enter one of the following environments: Alkaline, Hot, Cold, Acidic, Salty: ')
+    microbe_b_env = input('Please enter one of the following environments: Alkaline, Hot, Cold, Acidic, Salty: ') 
     #should be an integer value, from 1-100 
     microbe_b_colony = input('Please enter the size of your colony, size 1-1000: ')
     #a yes or a no
+    microbe_b_colony_size=colony_size.colony_growth()
     microbe_b_sec = input('Does your microbe have a secretion system? Yes or No?: ')
     #microbes are set
     print(f'Microbe A is {microbe_a_species} and Microbe B is {microbe_b_species}. Lets battle!')
